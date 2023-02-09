665839043657ad49dd3aac512f5d6a760ddf704c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-02-09 12:38:23 -0300

    feate:  implement productRouter in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import index from './routes';
+
+const { productsRouter } = index;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(productsRouter);
 
 export default app;
