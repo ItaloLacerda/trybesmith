25837fac6b14a94b081ef88d3285e41e7b227c68
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-02-14 12:40:13 -0300

    feat: creates "OrdersModel" class with "read" method

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,9 @@
+import OrdersModel from './orders.model';
 import ProductsModel from './products.model';
 import UsersModel from './users.model';
 
 export default {
   ProductsModel,
   UsersModel,
+  OrdersModel,
 };
