25c93b2ecc579a745a42b23b231a254a53e97848
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-02-14 10:45:18 -0300

    feat: create "UserModel" class with "create" method

---
+++ src/models/users.model.ts
@@ -0,0 +1,22 @@
+import { ResultSetHeader } from 'mysql2';
+import { Iuser } from '../interfaces/products.interface';
+import connection from './connection';
+
+class UsersModel {
+  static async create(newUser: Iuser) {
+    const columns = Object.keys(newUser).join(', ');
+  
+    const placeholders = Object.keys(newUser)
+      .map((_key) => '?')
+      .join(', ');
+  
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(
+      `INSERT INTO Trybesmith.products (${columns}) VALUE (${placeholders})`,
+      [...Object.values(newUser)],
+    );
+    
+    return insertId;
+  }
+}
+
+export default UsersModel;
