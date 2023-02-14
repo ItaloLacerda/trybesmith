bdd3df60d19adddfbb88eb8a05a71a88f5347c8c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-02-14 11:41:58 -0300

    feat: create "UserService" class with "create" method

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,7 @@
 import ProductsService from './products.service';
+import UsersService from './users.service';
 
 export default {
   ProductsService,
+  UsersService,
 };
---
+++ src/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Iuser } from '../interfaces/products.interface';
+import index from '../models';
+import jwt from '../utils/jsonWebToken/generateToken';
+
+const { UsersModel } = index;
+
+class ProductsService {
+  static async create(newUser: Iuser) {
+    const userId = await UsersModel.create(newUser);
+
+    const { level, username, vocation } = newUser;
+
+    if (userId) {
+      const tokenResponse = jwt.generateToken({ level, username, vocation });
+      return tokenResponse;
+    }
+  }
+}
+
+export default ProductsService;
